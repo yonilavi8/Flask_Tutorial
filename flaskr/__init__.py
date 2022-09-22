3ecd5ba6c97f7bf46654ded27cb5d3d9dea62b26
Yoni Lavi <yoni.lavi.17@ucl.ac.uk>
2022-09-22 16:45:40 +0100

    blog blueprint and index html file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -42,4 +42,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # import and register blog blueprint
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
