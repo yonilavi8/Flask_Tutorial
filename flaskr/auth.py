695bbd5f48f6574d2d1825c41799a2b3faa4ead9
Yoni Lavi <yoni.lavi.17@ucl.ac.uk>
2022-09-22 16:31:03 +0100

    removed typo in auth.py line 61

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -58,7 +58,7 @@ def register():
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-    if request.method() == 'POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
