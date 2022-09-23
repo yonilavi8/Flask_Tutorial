429b492548d85c0cfe7083fe4049cc4ede3d20ea
Yoni Lavi <yoni.lavi.17@ucl.ac.uk>
2022-09-23 14:28:20 +0100

    Installation, test setup and test fixtures

---
+++ setup.py
@@ -0,0 +1,12 @@
+from gettext import install
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
