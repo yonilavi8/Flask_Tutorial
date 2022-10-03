8c2ff7acd60e1aee4feb4a91cfdf1713f23c1c47
Yoni Lavi <yoni.lavi.17@ucl.ac.uk>
2022-10-03 12:18:21 +0100

    Posts output grouped by title

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -15,7 +15,7 @@ def index():
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' ORDER BY created ASC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
